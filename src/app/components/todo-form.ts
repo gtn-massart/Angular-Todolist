36cf8a8e7c069cab85c4028703b811c0f98bd5e3
Gaëtan Massart <gtn.massart@gmail.com>
2026-04-24 09:10:51 +0200

    input + button

--- src/app/components/todo-form.ts
+++ src/app/components/todo-form.ts
@@ -3,7 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-todo-form',
   imports: [],
-  template: ` <p>todo-form works!</p> `,
-  styles: ``,
+  template: `
+    <input type="text" class="flex-auto border" placeholder="Entrez une todo">
+    <button class="btn btn-primary">Ajouter</button>
+  `,
+  styles: `
+  :host{
+    display: flex;
+    gap: 12px;
+  }
+  `,
 })
 export class TodoForm {}

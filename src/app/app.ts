67758c6beb86c9212b619e1950c0afb026dd27f6
Gaëtan Massart <gtn.massart@gmail.com>
2026-04-24 08:02:44 +0200

    Mise en place

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [],
+  template:`<h1>Todo</h1>`,
+  styles:``
 })
 export class App {
   protected readonly title = signal('Angular-Todolist');

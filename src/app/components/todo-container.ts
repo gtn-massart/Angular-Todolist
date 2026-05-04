58df61d5bdea86aa1d7425f777c3e6c648c467de
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-04 10:26:24 +0200

    modify data resources

--- src/app/components/todo-container.ts
+++ src/app/components/todo-container.ts
@@ -1,15 +1,21 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TodoForm } from './todo-form';
 import { TodosList } from './todos-list';
-import { TodoFormInterface, TodoInterface } from '../shared/interfaces/todo.interface';
+import { TodoFormInterface } from '../shared/interfaces/todo.interface';
 import { TodosService } from '../shared/services/todos-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-container',
-  imports: [TodoForm, TodosList],
+  imports: [TodoForm, TodosList, JsonPipe],
   template: `
     <app-todo-form (addTodo)="addTodo($event)" />
-    <app-todos-list (toggleTodo)="toggleTodo($event)" [todosList]="todosList()" />
+    <app-todos-list
+      (toggleTodo)="toggleTodo($event)"
+      (selectTodo)="selectTodo($event)"
+      [todosList]="todosList()"
+    />
+    <pre>{{ selectedTodo() | json }}</pre>
   `,
   styles: `
     :host {
@@ -18,25 +24,16 @@ import { TodosService } from '../shared/services/todos-service';
   `,
 })
 export class TodoContainer {
-  todosList = signal<TodoInterface[]>([]);
   todosService = inject(TodosService);
+  todosList = computed(() => this.todosService.todosResource.value() || []);
+  selectedTodo = this.todosService.selectedTodoResource.value;
 
   addTodo(todo: TodoFormInterface) {
     this.todosService.addTodo(todo);
   }
 
-  toggleTodo(todoId: string) {
-    this.todosList.update((todos) =>
-      todos.map((todo) => {
-        if (todoId === todo._id) {
-          return {
-            ...todo,
-            done: !todo.done,
-          };
-        } else {
-          return todo;
-        }
-      }),
-    );
+  selectTodo(todoId: string) {
+    this.todosService.selectTodo(todoId);
   }
+  toggleTodo(todoId: string) {}
 }

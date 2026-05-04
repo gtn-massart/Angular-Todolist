b13e91b5ebcbad827faf963ed03979a9a08cbed6
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-04 10:27:52 +0200

    add  provide data resources

--- src/app/shared/services/todos-service.ts
+++ src/app/shared/services/todos-service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
-import { TodoFormInterface } from '../interfaces/todo.interface';
+import { Injectable, resource, signal } from '@angular/core';
+import { TodoFormInterface, TodoInterface } from '../interfaces/todo.interface';
+import { Todo } from '../../components/todo';
 
 @Injectable({
   providedIn: 'root',
@@ -7,7 +8,29 @@ import { TodoFormInterface } from '../interfaces/todo.interface';
 export class TodosService {
   BASE_URL = 'https://restapi.fr/api/atodos';
 
+  todosResource = resource({
+    loader: async (): Promise<TodoInterface[]> => (await fetch(this.BASE_URL)).json(),
+  });
+
+  selectedTodoId = signal<string | null>(null);
+
+  selectedTodoResource = resource({
+    params: this.selectedTodoId,
+    loader: async ({ params }): Promise<Todo | undefined> => {
+      if (params) {
+        return (await fetch(`${this.BASE_URL}/${params}`)).json();      
+      } else {
+        return;
+      }
+    } 
+  });
+
   constructor() {}
+
+  selectTodo(todoId: string) {
+    this.selectedTodoId.set(todoId);
+    console.log(this.selectedTodoId());
+  }
 
   async addTodo(todo: TodoFormInterface) {
     try {
@@ -25,7 +48,7 @@ export class TodosService {
         throw new Error('Oops');
       }
     } catch (e) {
-      throw new Error('Oops')
+      throw new Error('Oops');
     }
   }
 }

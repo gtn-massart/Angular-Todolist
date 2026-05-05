e92d3cf2b945bd1fccc0279d8df489804ed0ec90
Gaëtan Massart <gtn.massart@gmail.com>
2026-05-05 08:00:43 +0200

    implement httpRessource()

--- src/app/shared/services/todos-service.ts
+++ src/app/shared/services/todos-service.ts
@@ -1,6 +1,7 @@
 import { effect, Injectable, resource, signal } from '@angular/core';
 import { TodoFormInterface, TodoInterface } from '../interfaces/todo.interface';
 import { Todo } from '../../components/todo';
+import { httpResource } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -9,32 +10,53 @@ export class TodosService {
   BASE_URL = 'https://restapi.fr/api/atodos';
   selectedTodoId = signal<string | null>(null);
 
-  todosResource = resource({
-    loader: async (): Promise<TodoInterface[]> => {
-      // ↓ ?delay=3 simule un chargement plus long, ici 3 secondes
-      const response = await fetch(`${this.BASE_URL}?delay=3`);
-      if (!response.ok) {
-        throw new Error('Erreur lors du chaergement des tâches.');
-      } else {
-        return response.json();
-      }
-    },
-  });
+  // todosResource = resource({
+  //   loader: async (): Promise<TodoInterface[]> => {
+  //     // ↓ ?delay=3 simule un chargement plus long, ici 3 secondes
+  //     const response = await fetch(`${this.BASE_URL}?delay=1`);
+  //     if (!response.ok) {
+  //       throw new Error('Erreur lors du chaergement des tâches.');
+  //     } else {
+  //       return response.json();
+  //     }
+  //   },
+  // });
+
+  // ↑↓ reviens à écrire ce qu'il y a ci-dessus mais en utilisant httpResource
+  todosResource = httpResource<Todo[]>(() => ({
+    url: this.BASE_URL,
+    method: 'GET',
+    // body: '',
+    params: {
+      delay: 1
+    }
+  }), {
+    defaultValue: [],
+  })
 
   reloadTodos() {
     this.todosResource.reload();
   }
 
-  selectedTodoResource = resource({
-    params: () => ({ id: this.selectedTodoId() }),
-    loader: async ({ params: { id }, abortSignal, previous }): Promise<Todo | undefined> => {
-      if (id) {
-        return (await fetch(`${this.BASE_URL}/${id}`, { signal: abortSignal })).json();
-      } else {
-        return;
-      }
-    },
-  });
+  // selectedTodoResource = resource({
+  //   params: () => ({ id: this.selectedTodoId() }),
+  //   loader: async ({ params: { id }, abortSignal, previous }): Promise<Todo | undefined> => {
+  //     if (id) {
+  //       return (await fetch(`${this.BASE_URL}/${id}`, { signal: abortSignal })).json();
+  //     } else {
+  //       return;
+  //     }
+  //   },
+  // });
+
+  // ↑↓ reviens à écrire ce qu'il y a ci-dessus mais en utilisant httpResource
+  selectedTodoResource = httpResource<Todo | undefined>(
+    () => {
+      const todoId = this.selectedTodoId()
+      if(!todoId) return undefined;
+      return `${this.BASE_URL}/${todoId}`
+    } 
+  )
 
   constructor() {
     effect(() => {
